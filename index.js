6101b4ef1e474b4cc2919726ffc5a8b133dd8f83
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-05-29 15:23:19 -0500

    1

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
     let saveButton = document.querySelector(`#saveData`)
     
-    saveButton.addEventListener(`click`, async function(event) {
+    saveButton.addEventListener(`click`, async function(){
       
       let url = `/.netlify/functions/store_data?userName=${displayName}`
 
