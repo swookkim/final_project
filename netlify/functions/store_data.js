c68ca2357d7dd419730e58538395051b5d748863
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-05-29 12:22:45 -0500

    1

--- netlify/functions/store_data.js
+++ netlify/functions/store_data.js
@@ -5,19 +5,14 @@ let firebase = require(`./firebase`)
 // /.netlify/functions/create_comment?postId=xxxxxxxxx&userName=Brian&body=Tacos!
 exports.handler = async function(event) {
 
-  // get the three querystring parameters and store in memory
-  let postId = event.queryStringParameters.postId
-  let userName = event.queryStringParameters.userName
-  let body = event.queryStringParameters.body
 
   // establish a connection to firebase in memory
   let db = firebase.firestore()
 
   // create a new post, wait for it to return
-  await db.collection('comments').add({
-    postId: postId,
-    userName: userName,
-    body: body,
+  await db.collection('userData').add({
+    userName: user,
+    kimchiPremium: KoreaToGlobalYield,
     created: firebase.firestore.FieldValue.serverTimestamp()
   })
 
