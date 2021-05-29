8cdf46de72432130b36a9c4c9f75b73e86d0643f
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-05-29 16:09:04 -0500

    1

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
     <div class="font-bold text-3xl">Current ${globalPrice} price in the global</div>`)
 
     document.querySelector(`.get-coin`).insertAdjacentHTML (`afterend`,
-    `<button class="saveData border bg-purple-500 text-white rounded m-4 px-2 py-2">Save</button>`)
+    `<button class="saveData border bg-purple-500 text-white rounded px-2 py-2">Save</button>`)
 
     })
       
