d3823b34daaba1d05b87a5f8d911b751de52ffa8
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-05-29 14:24:57 -0500

    1

--- netlify/functions/store_data.js
+++ netlify/functions/store_data.js
@@ -11,7 +11,7 @@ exports.handler = async function(event) {
   let db = firebase.firestore()
 
   // create a new post, wait for it to return
-  let docRef = await db.collection('items').add({
+  await db.collection('items').add({
     userName: 'userName',
     kimchiPremium: 'kimchiPremium',
     created: firebase.firestore.FieldValue.serverTimestamp()
